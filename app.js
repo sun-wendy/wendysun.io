28fc7c449ca4a9e2c4efaf8837ab546f76ec4729
Wendy Sun <wendysun@mit.edu>
2023-07-20 18:43:08 +0800

    Created pages & added intro

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.menu');
-const sectBtn = document.querySelector('.control');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function pageTransitions(){
+    // Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let curBtn = document.querySelectorAll('.active-btn');
+            curBtn[0].className = curBtn[0].className.replace(' active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    // Section active class
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id){
+            // Remove selected from other buttons
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // Hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+pageTransitions();
